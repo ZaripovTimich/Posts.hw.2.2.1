5d5e38e5f5261eaf93d002fb9d17fa7dc13b14ac
Timur Zaripov <zaripov_t99@mail.ru>
2021-03-16 20:46:35 +0700

    test doesn't work

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -7,7 +7,33 @@ class WallServiceTest {
     @Test
     fun add() {
 
-        val expected = 2
+        val expected = 3
+
+        WallService.add(Post(
+            0,
+            10,
+            10,
+            10,
+            60,
+            "Всем привет!",
+            10,
+            143,
+            true,
+            Comments(100,groupsCamPost = true, canClose = true, canOpen = true, canPost = true),
+            Copyright(144,"Link", "Name", "Sport"),
+            Likes(300, userLikes = true, canLike = false, canPublish = true),
+            Reposts(50,true),
+            Views(400),
+            "post",
+            143,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = false,
+            isFavorite = false,
+            postponedId = 123
+        ))
 
         WallService.add(Post(
             0,
@@ -41,7 +67,7 @@ class WallServiceTest {
             10,
             10,
             60,
-            "Всем привет!",
+            "Я сделал дз",
             10,
             143,
             true,
@@ -60,48 +86,20 @@ class WallServiceTest {
             isFavorite = false,
             postponedId = 123
         ))
-
-
 
         assertEquals(expected, result.id)
     }
 
     @Test
-    fun update_TestFalse() {
+    fun update_TestTrue() {
 
-        WallService.add(Post(
-            0,
+        val result1 = WallService.update(Post(
+            1,
             10,
             10,
             10,
             60,
-            "Всем привет!",
-            10,
-            143,
-            true,
-            Comments(100, canPost = true, groupsCamPost = true, canClose = true, canOpen = true),
-            Copyright(144,"Link", "Name", "Sport"),
-            Likes(300, userLikes = true, canLike = false, canPublish = true),
-            Reposts(50,true),
-            Views(400),
-            "post",
-            143,
-            true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = false,
-            isFavorite = false,
-            postponedId = 123
-        ))
-
-        WallService.add(Post(
-            0,
-            10,
-            10,
-            10,
-            60,
-            "Как дела?",
+            "Я сделал дз",
             10,
             143,
             true,
@@ -118,11 +116,16 @@ class WallServiceTest {
             isPinned = true,
             markedAsAds = false,
             isFavorite = false,
-            postponedId = 123
-        ))
+            postponedId = 123))
+
+        assertTrue(result1)
+    }
+
+    @Test
+    fun update_TestFalse() {
 
         val result = WallService.update(Post(
-            3,
+            4,
             10,
             10,
             10,
@@ -149,86 +152,5 @@ class WallServiceTest {
         assertFalse(result)
     }
 
-    @Test
-    fun update_TestTrue() {
 
-        WallService.add(Post(
-            0,
-            10,
-            10,
-            10,
-            60,
-            "Всем привет!",
-            10,
-            143,
-            true,
-            Comments(100, canPost = true, groupsCamPost = true, canClose = true, canOpen = true),
-            Copyright(144,"Link", "Name", "Sport"),
-            Likes(300, userLikes = true, canLike = false, canPublish = true),
-            Reposts(50,true),
-            Views(400),
-            "post",
-            143,
-            true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = false,
-            isFavorite = false,
-            postponedId = 123
-        ))
-
-        WallService.add(Post(
-            0,
-            10,
-            10,
-            10,
-            60,
-            "Как дела?",
-            10,
-            143,
-            true,
-            Comments(100,groupsCamPost = true, canClose = true, canOpen = true, canPost = true),
-            Copyright(144,"Link", "Name", "Sport"),
-            Likes(300, userLikes = true, canLike = false, canPublish = true),
-            Reposts(50,true),
-            Views(400),
-            "post",
-            143,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = false,
-            isFavorite = false,
-            postponedId = 123
-        ))
-
-        val result = WallService.update(Post(
-            1,
-            10,
-            10,
-            10,
-            60,
-            "Я сделал дз",
-            10,
-            143,
-            true,
-            Comments(100,groupsCamPost = true, canClose = true, canOpen = true, canPost = true),
-            Copyright(144,"Link", "Name", "Sport"),
-            Likes(300, userLikes = true, canLike = false, canPublish = true),
-            Reposts(50,true),
-            Views(400),
-            "post",
-            143,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = false,
-            isFavorite = false,
-            postponedId = 123))
-
-        assertTrue(result)
-    }
 }
